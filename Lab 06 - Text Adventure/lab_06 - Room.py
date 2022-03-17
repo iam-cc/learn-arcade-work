649d2bc46629b2ee583c8405d64d6c4fd9e5133f
WSU_Student <chaerinyeom@mail.weber.edu>
2022-03-16 23:27:55 -0600

    Lab 06

--- Lab 06 - Text Adventure/lab_06 - Room.py
+++ Lab 06 - Text Adventure/lab_06 - Room.py
@@ -2,42 +2,13 @@ def start():
     print('''
 ****************************************
 *                                      *
-*  Welcome to Chaerin's Text Adventure!  *
+*  Welcome to Chaerin's Text Adventure!*
 *                                      *
 ****************************************
 ''')
-    global gold
-    gold = 2
-    global health
-    health = 10
-    global energy
-    energy = 10
-    global melee
-    melee = 1
-    global ranged
-    ranged = 0
-    global ammo
-    ammo = 0
-    global fullhealth
-    fullhealth = 10
-    global attacknum
-    attacknum = 0
+
     balcony()
 
-## Game functions
-
-def checkstats():
-    print('''
-
-********''')
-    print("Gold:", gold)
-    print("Health:", health,"/",fullhealth)
-    print("Melee:", melee)
-    print ("Energy:", energy)
-    global ranged
-    if ranged > 0:
-        print("Ranged:", ranged)
-        print("Ammo:", ammo)
 
 def prompt():
     x = input("Type a command: ")
@@ -45,21 +16,21 @@ def prompt():
 
 
 
-##Rooms*********************************************************
 
+# ROOMS*******************************************************************************8
 
-## Balcony////
+# Balcony
 
 def balcony():
     print("****************************************")
     print('''
 
-You are in your balcony in a small home.
+You are on the balcony in your house. The wind is strong and cold.
     ''')
     print('''Options:
-1. Go Inside
-2. Rest
-9. Stats
+1. Go inside the house
+2. Stay on the balcony
+
 ''')
 
     command = prompt()
@@ -67,30 +38,24 @@ You are in your balcony in a small home.
         north_hall()
     elif command == "2":
         print("********")
-        print ("You are back to full health. (",fullhealth,"/",fullhealth,")")
-        global health
-        health = fullhealth
-        balcony()
-    elif command == "9":
-        checkstats()
+        print ("You are still on the balcony. It's getting colder.")
         balcony()
     else:
         balcony()
 
 
-## north_hall ////
+# north_hall
 def north_hall():
     print("****************************************")
     print ('''
-
-You are in your north hall. You can walk around the small home.
+You are in your north hall. You can walk around your house.
     ''')
 
     print('''Options:
 1. Go back to the balcony
-2. Enter the bedroom
-3. Walk to the coast
-9. Stats
+2. Enter the bedroom on your right
+3. Enter the kitchen on your left
+4. Walk forward
 ''')
     command = prompt()
     if command == "1":
@@ -99,207 +64,184 @@ You are in your north hall. You can walk around the small home.
         bedroom()
     elif command == "3":
         kitchen()
-    elif command == "9":
-        checkstats()
-        north_hall()
+    elif command == "4":
+        south_hall()
     else:
         north_hall()
 
+# south_hall
+def south_hall():
+    print("****************************************")
+    print ('''
+You are in your south hall. You can walk around your house.
+    ''')
 
+    print('''Options:
+1. Go back to the north hall
+2. Enter the bathroom on your right
+3. Enter the dinning room on your left
+''')
+    command = prompt()
+    if command == "1":
+        north_hall()
+    elif command == "2":
+        bathroom()
+    elif command == "3":
+        dinning()
+    else:
+        south_hall()
 
+# Dinning room
+def dinning():
+    print('''****************************************
+You are in the dinning room. Nothing in the dinning room.
+    ''')
+    print('''Options:
+    1. Stay in the dinning room
+    2. Go back to the south hall
+''')
 
+    command = prompt()
+    if command == "1":
+        dinning()
+    elif command == "2":
+        south_hall()
+    else:
+        south_hall()
 
+# Bathroom
+def bathroom():
+    print('''****************************************
+You are in the bathroom. There is warm water in the bathtub.
+    ''')
+    print('''Options:
+    1. Take a bath
+    2. Go back to the south hall
+''')
 
+    command = prompt()
+    if command == "1":
+        bath()
+    elif command == "2":
+        south_hall()
+    else:
+        south_hall()
 
+# kitchen
+def kitchen():
+    print('''****************************************
+You are in the kitchen. It smells like pancakes.
+    ''')
+    print('''Options:
+    1. Eat pancakes 
+    2. Go back to the north hall
+''')
 
-## Bedroom ////
+    command = prompt()
+    if command == "1":
+        pancakes()
+    elif command == "2":
+        north_hall()
+    else:
+        kitchen()
+
+# Bedroom
 def bedroom():
     print('''****************************************
 
 You are in the bedroom. It's dark.
-    ''')
+''')
+    print('''Options:
+    1. Turn on the light 
+    2. Go back to the north hall
+''')
 
-    ## This is what happens when you enter the forest for the first time
-    global attacknum
-    if attacknum == 0:
-        print('''********
-You are attacked by a wild beast! OH NO!!
-(-1 health)
-
-
-''')### It says that you have been attacked
-        attacknum = 1 #It records that you have been attacked
-        global health
-        health = health - 1#Your health goes down
-
-
-    ##This is what happens when you have already entered the bedroom before
-    if attacknum == 1:
-        print('''Options:
-1. Go to your north_hall
-2. Pick up stones
-3. Explore deeper into the forest
-9. Stats
-    ''') ##It gives you your options
-        command = prompt()
-        if command == ("1"):
-          north_hall()
-        elif command == ("2"):
-          find_stones()
-          forest()
-        elif command == "3":
-            battle()
-        elif command == "9":
-          checkstats()
-          bedroom()
-        else:
-          bedroom()
-
-
-
-
-
-
-## Kitchen
-def kitchen():
-    print ('''****************************************
-
-You at the kitchen. It smells lie pancakes.
-    ''')
-    print ('''Options:
-1. Go to north hall
-2. Talk to fisherman
-3. Pick up stones
-9. Stats''')
     command = prompt()
     if command == "1":
+        light_on()
+    elif command == "2":
         north_hall()
-    elif command == "2":
-        fishconvo()
-    elif command =="3":
-        find_stones()
-        kitchen()
-    elif command =="9":
-        checkstats()
-        kitchen()
     else:
-        kitchen()
+        bedroom()
 
 
-##City
-def city():
-        print('''****************************************
-    You arrived!!!!!
-         ''')
+# EVENTS *****************************************************************************************
+# turn on the light
+def light_on():
+    print('''****************************************
 
-
-## Events ***********************************************************
-def find_stones():
-    global ranged
-    global ammo
-    if ranged == 0:
-        print('''********
-
-You found 5 stones you can throw at an enemy.
-(+1 ranged) (+5 ammo) ''')
-        ranged = 1
-        ammo = 5
-
-    elif ranged >=1 and ammo < 5:
-        ammo = 5
-        print('''********
-You stuff a few stones in your bag for later.''')
-    elif ranged >= 1 and ammo > 4:
-        print('''
-********
-        You don't find any suitable stones.''')
-
-
-
-
-
-
-
-## Battle//////////
-def battle():
-    print('''****************
-
-You have been attacked by a wild pig chicken. What do you do?
+You turn on the light. There are some clothes to prevent freezing.
+    ''')
+    print('''Options:
+    1. Put on clothes 
+    2. Go back to the north hall without clothes
 ''')
 
-    print('''1. Run
-2. Fight
-3. Make friends with the pig chicken''')
     command = prompt()
     if command == "1":
-        forest()
+        clothes()
     elif command == "2":
-        print('''
-*********''')
-        print("You have just been brutally raped by a horny pig chicken!! You suck!!")
-        print('''
-
-GAME OVER!!!!!''')
-    elif command == "3":
-        print('''
-*********''')
-        print("You have just been brutally raped by a horny pig chicken!! You suck!!")
-        print('''
-
-
-GAME OVER!!!!!''')
-
-
-
-##Training//////////////
-def fishconvo():
-    print('''********
-
-Fisherman: "The fish aren't biting today. Want me to teach you a few boxing moves?
-
-1. Yes
-2. No''')
-    global melee
-    command=prompt()
-    if command == "1" and melee <2:
-        melee = 2
-        print('''********
-Fisherman: "Now don't go beating up that nerdy crippled kid."
-(+1 melee)
-''')
-        coast()
-    elif command == "1" and melee >1:
-        print('''********
-Fisherman "Looks like I have nothing left to teach you."
-        ''')
-        coast()
-    elif command == "2":
-        print(''' ********
-Fisherman: "Well, don't come crying to me if you get brutally raped by a horny pig chicken."
-''')
-        coast()
+        north_hall()
     else:
-        coast()
+        light_on()
 
 
-def boatconvo():
-    print('''********
-Boat captain: "I can sail you to the city for a nominal fee."
+#clothes
+def clothes():
+    print('''****************************************
 
-1. Okay
-2. No thanks
+You put on clothes. Your body is getting warm.
+    ''')
+    print('''Options:
+    1. Wear some other clothes
+    2. Go back to the north hall without clothes
 ''')
+
     command = prompt()
     if command == "1":
-        print('''********
-The boatman sails you to the capital city.
-(-2 gold)
+        clothes()
+    elif command == "2":
+        north_hall()
+    else:
+        clothes()
+
+
+# pancakes
+def pancakes():
+    print('''****************************************
+
+You eat pancakes. You are getting full.
+    ''')
+    print('''Options:
+    1. Eat more pancakes 
+    2. Go back to the north hall
 ''')
-        global gold
-        gold = gold -2
-        city()
-    elif command =="2":
-        coast()
+
+    command = prompt()
+    if command == "1":
+        pancakes()
+    elif command == "2":
+        north_hall()
+    else:
+        pancakes()
+
+# bath
+def bath():
+    print('''****************************************
+
+You take a bath. It makes you feel better.
+    ''')
+    print('''Options:
+    1. Go back to the south hall
+''')
+
+    command = prompt()
+    if command == "1":
+        south_hall()
+    else:
+        bath()
+
+
 
 
 start()
